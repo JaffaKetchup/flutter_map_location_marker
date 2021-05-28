75d8aa5815d3b66dd3859c0f546991f7af69226e
Luka S <58115698+JaffaKetchup@users.noreply.github.com>
2021-05-28 18:07:47 +0100

    Update flutter_map_location_marker.dart

--- lib/flutter_map_location_marker.dart
+++ lib/flutter_map_location_marker.dart
@@ -8,7 +8,7 @@ import 'package:flutter_compass/flutter_compass.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:latlong/latlong.dart';
+import 'package:latlong2/latlong.dart';
 
 import 'src/position_tween.dart';
 
